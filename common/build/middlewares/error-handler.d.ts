b4a5da003bcdfb4ffa7e6469858cb09bab2f4e17
ironhight <mrnguyenhoainam2468@gmail.com>
2021-02-13 17:48:53 +0700

    ci build

--- common/build/middlewares/error-handler.d.ts
+++
@@ -1,3 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-import { CustomError } from '../errors/custom-error';
-export declare const errorHandler: (err: CustomError, req: Request, res: Response, next: NextFunction) => Response<any, Record<string, any>> | undefined;
